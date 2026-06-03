287bb7e195623ff4c20480fc2aa8284e3bfa02ea
vikram <vikram38009@gmail.com>
2026-06-03 17:51:16 +0530

    fix: add STATIC_ROOT for collectstatic on Render

--- backend/jobtracker/settings.py
+++ backend/jobtracker/settings.py
@@ -145,7 +145,8 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
